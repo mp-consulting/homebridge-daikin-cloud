cd7cc2e26bb92a54d1a4d4c1896e325f32c0a4ba
mp-consulting <mickael.palma@mac.com>
2026-01-08 14:34:35 +0000

    chore: rename package to @mp-consulting/homebridge-daikin-cloud

--- src/settings.ts
+++ src/settings.ts
@@ -6,4 +6,4 @@ export const PLATFORM_NAME = 'DaikinCloud';
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'homebridge-daikin-cloud';
+export const PLUGIN_NAME = '@mp-consulting/homebridge-daikin-cloud';
